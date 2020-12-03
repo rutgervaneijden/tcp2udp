66947f5039a88f8f49b2e51dcfbb938a8dc8cac0
rutgervaneijden <63955794+rutgervaneijden@users.noreply.github.com>
2020-12-03 07:04:47 +0100

    Add files via upload

---
+++ tcp2udp.py
@@ -0,0 +1,18 @@
+import socket
+
+TCP_port = 55551
+UDP_address = '127.0.0.1'
+UDP_port = 55552
+
+TCP = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+TCP.bind(('',TCP_port))
+
+UDP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+TCP.listen(1)
+connection,address = TCP.accept()
+
+while True:
+    data = connection.recv(1024)
+    UDP.sendto(data, (UDP_address, UDP_port))
+    print(data)    
