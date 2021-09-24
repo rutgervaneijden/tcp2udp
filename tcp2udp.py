1c2b8a8ed486b996a6870df35ac4e663f9553d56
rutgervaneijden <63955794+rutgervaneijden@users.noreply.github.com>
2021-09-24 08:55:48 +0200

    Update tcp2udp.py

--- tcp2udp.py
+++ tcp2udp.py
@@ -1,20 +1,25 @@
-# 2021 Rutger van Eijden
-
 import socket
 
+TCP_host = '127.0.0.1'
 TCP_port = 55551
-UDP_address = '127.0.0.1'
+
+UDP_host = '127.0.0.1'
 UDP_port = 55552
 
+sentences = ['RSA','THS','VDM','VTG']
+
+def filter(data):
+    if data[3:6].decode() in sentences:
+        UDP.sendto(data, (UDP_host, UDP_port))
+        print(data)
+
 TCP = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-TCP.bind(('',TCP_port))
-
-UDP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
+TCP.bind((TCP_host,TCP_port))
 TCP.listen(1)
 connection,address = TCP.accept()
 
+UDP = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
 while True:
     data = connection.recv(1024)
-    UDP.sendto(data, (UDP_address, UDP_port))
-    print(data)    
+    filter(data)
